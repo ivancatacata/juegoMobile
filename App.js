70631eadbf1ee46553ac7ebe59a9c4e3d94ff27f
ivan catacata <catacata.ivan@gmail.com>
2022-09-21 17:58:35 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { useFonts } from "expo-font";
+import Header from "./components/header";
+import GameScreen from "./screens/game";
+import StartGameScreen from "./screens/start-game";
+import { colors } from "./constants/colors";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  containerLoader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
 });
+
+export default function App() {
+  const [userNumber, setUserNumber] = useState(0);
+  const [loaded] = useFonts({
+    "Rajdhani-Regular": require("./assets/fonts/Rajdhani-Regular.ttf"),
+    "Rajdhani-Bold": require("./assets/fonts/Rajdhani-Bold.ttf"),
+    "Rajdhani-Light": require("./assets/fonts/Rajdhani-Light.ttf"),
+    "Rajdhani-Italic": require("./assets/fonts/Rajdhani-Italic.ttf"),
+    "Rajdhani-SemiBold": require("./assets/fonts/Rajdhani-SemiBold.ttf"),
+  });
+  const title = !userNumber ? "Adivina un numero" : "Comienza el juego";
+
+  const onStartGame = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  if (!loaded) {
+    return (
+      <View style={styles.containerLoader}>
+        <ActivityIndicator size="large" color={colors.primary} />
+      </View>
+    );
+  }
+
+  let content = <StartGameScreen onStartGame={onStartGame} />;
+
+  if (userNumber) {
+    content = <GameScreen selectedNumber={userNumber} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header title={title} />
+      {content}
+    </View>
+  );
+}
---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: "#89fcb3",
+  secondary: "#ff0051",
+  text: "#212121",
+  background: "#ffffff",
+  backgroundLight: "#F5EFED",
+  backgroundDark: "#010d23",
+  white: "#ffffff",
+  black: "#000000",
+  gray: "#a69a90",
+};
